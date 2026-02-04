e00093336b50a6dcc586acb278c7645e31544725
ghustdev <gustavocardoso0019@gmail.com>
2026-02-04 15:25:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+void main() {
+	IO.println(String.format("Hello and welcome!"));
+	System.out.println("Welcome to the main program!");
+	IO.println("Hello World!");
+	
+	for (int i = 1; i <= 5; i++) {
+		IO.println("i = " + i);
+	}
+}
