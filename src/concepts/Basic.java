a39cdd0597b9d6487a2682185fd3c3025d55df3c
ghustdev <gustavocardoso0019@gmail.com>
2026-02-09 08:11:26 -0300

    Feat: learn Java concepts

---
+++ src/concepts/Basic.java
@@ -0,0 +1,7 @@
+package concepts; // Agrupamento de Classes, podendo ser as entities, services, repositories...
+
+public class Basic {
+	static void main() {
+		System.out.println("Hello World");
+	}
+}
